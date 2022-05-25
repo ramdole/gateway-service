89db9e449668c4933c200db226f991720e0d04a4
ramdole <ramdole@amorepacific.com>
2022-05-25 18:15:13 +0900

    Initial commit

---
+++ src/main/java/com/example/gatewayservice/config/filter/LoggingFilter.java
@@ -0,0 +1,50 @@
+package com.example.gatewayservice.config.filter;
+
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.OrderedGatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.core.Ordered;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+@Component
+@Slf4j
+public class LoggingFilter extends AbstractGatewayFilterFactory<LoggingFilter.Config> {
+
+  public LoggingFilter() {
+    super(Config.class);
+  }
+
+  @Override
+  public GatewayFilter apply(Config config) {
+
+    return new OrderedGatewayFilter((exchange, chain) -> {
+      ServerHttpRequest request = exchange.getRequest();
+      ServerHttpResponse response = exchange.getResponse();
+
+      log.info("Logging Pre Filter Base message : {}", config.getBaseMessage());
+
+      if(config.isPreLogger()) {
+        log.info("Logging Pre Filter : {}", request.getId());
+      }
+      return chain.filter(exchange).then(Mono.fromRunnable(() -> {
+        if(config.isPostLogger()) {
+          log.info("Logging Post Filter end : {}", response.getStatusCode());
+        }
+      }));
+
+    }, Ordered.LOWEST_PRECEDENCE);
+  }
+
+  @Data
+  public static class Config {
+
+    private String baseMessage;
+    private boolean preLogger;
+    private boolean postLogger;
+  }
+}
